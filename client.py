e22f761c8f72a22c127364ddc8b40b0fa84530ab
Egor Eremeev <egor.b.eremeev@gmail.com>
2020-11-09 16:04:13 +0200

    Working Example

--- client.py
+++ client.py
@@ -1,57 +1,24 @@
 import grpc
+import logging
 
-from generated import purchase_campaign_pb2_grpc, purchase_campaign_pb2, test_pb2_grpc, test_pb2
+from generated import test_pb2_grpc, test_pb2
 
 
-def client():
+def run():
     with grpc.insecure_channel('localhost:6066') as conn:
-        stub = purchase_campaign_pb2_grpc.PurchaseCampaingStub(conn)
-        # stub = test_pb2_grpc.TestStub(conn)
-        # foo = test_pb2.Foo()
-        # bar = foo.bars.add()  # Adds a Bar then modify
-        # bar.i = 15
-        # bar.j = 12
+        stub = test_pb2_grpc.TestStub(conn)
 
-        # foo = test_pb2.Foo(  # Creates Foo
-        #     bars=[  # with its field bars set to a list
-        #         test_pb2.Bar(i=15, j=17),  # where each list member is also initialized during creation.
-        #         test_pb2.Bar(i=32),
-        #         test_pb2.Bar(i=47, j=77),
-        #     ]
-        # )
-        # stub = purchase_campaign_pb2_grpc.PurchaseCampaingStub(conn)
+        helloRepeatedRequest = test_pb2.HelloRepeatedRequest()
+        helloRepeatedRequest.name = 'gfhdhgfdh'
+        helloRepeatedRequest.bars.add().i = 15
+        helloRepeatedRequest.bars.add().i = 16
+        helloRepeatedRequest.bars.add().i = 17
+        print("TestRepeate client prepared: " + str(helloRepeatedRequest))
 
-        # rq.suppliers_to_request.id = '1'
-
-        # sup = rq.suppliers_to_request.add()
-        # print(sup)
-        # sup.id = '1'
-        # del rq.suppliers_to_request[:]
-        # rq.suppliers_to_request.extend(sup)
-
-        # sup = purchase_campaign_pb2.SupplierToRequest()
-        # sup.id = '1'
-        # sup.contacts['full_name'] = "test"
-        # sup.contacts['company_name'] = "test"
-        # sup.contacts['email'] = "test"
-        # sup.metadata['info'] = "test"
-
-        # rq.suppliers_to_request.MergeFrom(sup)
-        # rq.suppliers_to_request.contacts['full_name'] = sup.contacts['full_name']
-        # rq.suppliers_to_request.contacts['company_name'] = sup.contacts['company_name']
-        # rq.suppliers_to_request.contacts['email'] = sup.contacts['email']
-        # rq.suppliers_to_request.metadata['info'] = sup.metadata['info']
-
-        rq = purchase_campaign_pb2.PurchaseCampaignVerified()
-        rq.id = 1
-        rq.anfrage.id = 1
-        rq.anfrage.name = 'product1'
-        rq.schedule.start_at = '12-12-2010'
-        rq.schedule.end_at = '12-12-2010'
-
-        response = stub.CreatePurchaseCampanies(rq)
-        print(response)
+        response = stub.SayRepeated(helloRepeatedRequest)
+        print("TestRepeate client received: " + str(response))
 
 
 if __name__ == "__main__":
-    client()
+    logging.basicConfig()
+    run()
--- server.py
+++ server.py
@@ -2,47 +2,30 @@ import time
 from concurrent import futures
 
 import grpc
-from generated import purchase_campaign_pb2, purchase_campaign_pb2_grpc, test_pb2, test_pb2_grpc
+from generated import test_pb2, test_pb2_grpc
 
+class TestServicer(test_pb2_grpc.TestServicer):
+    def SayRepeated(self, request, context):
+        print(f'TestServicer has got this request with repeated Message as field: {request}')
 
-class PurchaseCompaingServicer(purchase_campaign_pb2_grpc.PurchaseCampaingServicer):
+        response = test_pb2.HelloRepeatedReply()
+        response.name = 'adsfafsad'
+        response.bars.add().i=25
+        response.bars.add().i = 26
+        response.bars.add().i = 27
 
-    def CreatePurchaseCampanies(self, request, context):
-        response = purchase_campaign_pb2.PurchaseCampaignVerified()
-        response.id = request.id
-        response.anfrage.id = request.anfrage.id
-        response.anfrage.name = request.anfrage.name
-        # response.suppliers_to_request = request.suppliers_to_request
-        # response.suppliers_to_request.contacts = request.suppliers_to_request.contacts
-        # response.suppliers_to_request.contacts['company_name'] = request.suppliers_to_request.contacts['company_name']
-        # response.suppliers_to_request.contacts['email'] = request.suppliers_to_request.contacts['email']
-        # response.suppliers_to_request.metadata['info'] = request.suppliers_to_request.metadata['info']
-        response.schedule.start_at = request.schedule.start_at
-        response.schedule.end_at = request.schedule.end_at
-
-        return response
-
-
-class TestServicer(purchase_campaign_pb2_grpc.PurchaseCampaingServicer):
-    def CreateFoo(self, request, context):
-        response = test_pb2.Foo()
-        response.i = request.bars
+        print(f'TestServicer has prepared this response with repeated Message as field: {response}')
 
         return response
 
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
-    purchase_campaign_pb2_grpc.add_PurchaseCampaingServicer_to_server(PurchaseCompaingServicer(), server)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
     test_pb2_grpc.add_TestServicer_to_server(TestServicer(), server)
     print('Starting server on port 6066.')
     server.add_insecure_port('[::]:6066')
     server.start()
-    try:
-        while True:
-            time.sleep(3600)
-    except KeyboardInterrupt:
-        server.stop(0)
+    server.wait_for_termination()
 
 
 if __name__ == '__main__':
